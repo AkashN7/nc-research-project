a54417b1204591f1bd7eb437e887371100893845
Akash C Nallani <anallani@umich.edu>
2021-07-22 21:30:41 -0400

    Task 2 Update

--- task2.py
+++ task2.py
@@ -12,19 +12,6 @@ d = {'LinkName': [], 'Year': [], 'Day-of-the-week': [], 'Time-of-the-day': [], '
      'TT10': [], 'TT15': [], 'TT25': [], 'TT50': [], 'AvgTT': [], 'TT85': [], 'TT90': [], 'TT95': [],
      'MaxTT': [], 'StdDev': []}
 
-# for i in range(1, 5):
-#     ln = 'Link' + str(i)
-#     for j in range(7, 10):
-#         yr = '201' + str(j)
-#         for k in range(1, 8):
-#             for x in range(0, 24):
-#                 arr = []
-#                 if x != 23:
-#                     time = str(x) + ':00-' + str(x+1) + ':00'
-#                 elif x == 23:
-#                     time = '23:00-0:00'
-#                 while
-
 d2 = {}
 
 for index, row in xl.iterrows():
@@ -41,17 +28,17 @@ for x in d2:
     d['Year'].append(labels[1])
     d['Day-of-the-week'].append(labels[2])
     d['Time-of-the-day'].append(labels[3])
-    d['MinTT'].append(np.min(d2[x]))
-    d['TT10'].append(np.percentile(d2[x], 10))
-    d['TT15'].append(np.percentile(d2[x], 15))
-    d['TT25'].append(np.percentile(d2[x], 25))
-    d['TT50'].append(np.percentile(d2[x], 50))
-    d['AvgTT'].append(np.mean(d2[x]))
-    d['TT85'].append(np.percentile(d2[x], 85))
-    d['TT90'].append(np.percentile(d2[x], 90))
-    d['TT95'].append(np.percentile(d2[x], 95))
-    d['MaxTT'].append(np.max(d2[x]))
-    d['StdDev'].append(np.std(d2[x]))
+    d['MinTT'].append(round(np.min(d2[x]), 2))
+    d['TT10'].append(round(np.percentile(d2[x], 10), 2))
+    d['TT15'].append(round(np.percentile(d2[x], 15), 2))
+    d['TT25'].append(round(np.percentile(d2[x], 25), 2))
+    d['TT50'].append(round(np.percentile(d2[x], 50), 2))
+    d['AvgTT'].append(round(np.mean(d2[x]), 2))
+    d['TT85'].append(round(np.percentile(d2[x], 85), 2))
+    d['TT90'].append(round(np.percentile(d2[x], 90), 2))
+    d['TT95'].append(round(np.percentile(d2[x], 95), 2))
+    d['MaxTT'].append(round(np.max(d2[x]), 2))
+    d['StdDev'].append(round(np.std(d2[x]), 2))
 
 frame = pd.DataFrame(d)
 frame.to_csv("ProcessedTask2.csv", index=False)
