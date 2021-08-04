fd065f8a5e77e447d8f1cca30472793ca0c4da70
rkpulugurtha <rkpulugurtha@gmail.com>
2021-08-04 15:59:38 -0400

    Task 3 Done (8/4/21)

--- task2.py
+++ task2.py
@@ -10,7 +10,8 @@ xl = pd.read_csv(data)
 
 d = {'LinkName': [], 'Year': [], 'Day-of-the-week': [], 'Time-of-the-day': [], 'MinTT': [],
      'TT10': [], 'TT15': [], 'TT25': [], 'TT50': [], 'AvgTT': [], 'TT85': [], 'TT90': [], 'TT95': [],
-     'MaxTT': [], 'StdDev': []}
+     'MaxTT': [], 'StdDev': [], 'PT': [], 'PTI': [], 'BT': [], 'BTI': [], 'TTI': [],
+     'L_Skew': [], 'L_Var': [], }
 
 d2 = {}
 
@@ -29,16 +30,34 @@ for x in d2:
     d['Day-of-the-week'].append(labels[2])
     d['Time-of-the-day'].append(labels[3])
     d['MinTT'].append(round(np.min(d2[x]), 2))
-    d['TT10'].append(round(np.percentile(d2[x], 10), 2))
+    P10 = np.percentile(d2[x], 10)
+    d['TT10'].append(round(P10, 2))
     d['TT15'].append(round(np.percentile(d2[x], 15), 2))
     d['TT25'].append(round(np.percentile(d2[x], 25), 2))
-    d['TT50'].append(round(np.percentile(d2[x], 50), 2))
-    d['AvgTT'].append(round(np.mean(d2[x]), 2))
+    P50 = np.percentile(d2[x], 50)
+    d['TT50'].append(round(P50, 2))
+    AVG = np.mean(d2[x])
+    d['AvgTT'].append(round(AVG, 2))
     d['TT85'].append(round(np.percentile(d2[x], 85), 2))
-    d['TT90'].append(round(np.percentile(d2[x], 90), 2))
+    P90 = np.percentile(d2[x], 90)
+    d['TT90'].append(round(P90, 2))
     d['TT95'].append(round(np.percentile(d2[x], 95), 2))
     d['MaxTT'].append(round(np.max(d2[x]), 2))
     d['StdDev'].append(round(np.std(d2[x]), 2))
 
+    # Task 3 stuff
+    PT = np.percentile(d2[x], 95)
+    d['PT'].append(round(PT, 2))
+    d['PTI'].append(round(PT/np.percentile(d2[x], 15), 2))
+    BT = PT - AVG
+    d['BT'].append(round(BT, 2))
+    d['BTI'].append(round(BT/AVG, 2))
+    d['TTI'].append(round(AVG/np.percentile(d2[x], 15), 2))
+    if P50-P10 == 0:
+        d['L_Skew'].append(float('inf'))
+    else:
+        d['L_Skew'].append(round((P90 - P50) / (P50 - P10), 2))
+    d['L_Var'].append(round((P90 - P10)/P50, 2))
+
 frame = pd.DataFrame(d)
-frame.to_csv("ProcessedTask2.csv", index=False)
+frame.to_csv("ProcessedTask3.csv", index=False)
